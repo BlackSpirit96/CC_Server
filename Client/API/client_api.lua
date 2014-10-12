bcb6c7dadce68fa4f5c207d781352156f37a7199
BlackSpirit <panagiotis_11@hotmail.com>
2014-10-12 22:43:02 +0300

    Fix Client API

--- Client/API/client_api.lua
+++ Client/API/client_api.lua
@@ -9,144 +9,127 @@ net:setProtocol("DPRK_SERVER")
 
 -- Account API
 
-function login(username, password)
-	net:send('login~'..username..'~'..password)
-	local message, distance, sender = net:receive()
-	return message
-end
-
-function changePassword(authToken, oldPassword, newPassword)
-	net:send('changePassword~'..authToken..'~'..oldPassword..'~'..newPassword)
-	local message, distance, sender = net:receive()
-	return message
-end
-
-function updateProfile(authToken, password, data)
-	net:send('updateProfile~'..authToken..'~'..password..'~'..data)
-	local message, distance, sender = net:receive()
-	return message
-end
-
-function showProfile(authToken, username)
-	net:send('showProfile~'..authToken..'~'..username)
-	local message, distance, sender = net:receive()
-	return message
-end
-
-function addAccount(authToken, password, username, userPassword, userLVL)
-	net:send('addAccount~'..authToken..'~'..password..'~'..username..'~'..userPassword..'~'..userLVL)
-	local message, distance, sender = net:receive()
-	return message
-end
-
-function removeAccount(authToken, password, username)
-	net:send('removeAccount~'..authToken..'~'..password..'~'..username)
-	local message, distance, sender = net:receive()
-	return message
-end
-
-function changeProfile(authToken, password, username, data)
-	net:send('changeProfile~'..authToken..'~'..password..'~'..username..'~'..data)
-	local message, distance, sender = net:receive()
-	return message
-end
-
-function changeUserPassword(authToken, password, username, newPassword)
-	net:send('changeUserPassword~'..authToken..'~'..password..'~'..username..'~'..newPassword)
-	local message, distance, sender = net:receive()
-	return message
-end
-
-function authTokenLvl(authToken)
-	net:send('authTokenLvl~'..authToken)
-	local message, distance, sender = net:receive()
-	return message
-end
+local account = {
+	login = function(username, password)
+		net:send('login~'..username..'~'..password)
+		local message, distance, sender = net:receive()
+		return message
+	end,
+	changePassword = function(authToken, oldPassword, newPassword)
+		net:send('changePassword~'..authToken..'~'..oldPassword..'~'..newPassword)
+		local message, distance, sender = net:receive()
+		return message
+	end,
+	updateProfile = function(authToken, password, data)
+		net:send('updateProfile~'..authToken..'~'..password..'~'..data)
+		local message, distance, sender = net:receive()
+		return message
+	end,
+	showProfile = function(authToken, username)
+		net:send('showProfile~'..authToken..'~'..username)
+		local message, distance, sender = net:receive()
+		return message
+	end,
+	addAccount = function(authToken, password, username, userPassword, userLVL)
+		net:send('addAccount~'..authToken..'~'..password..'~'..username..'~'..userPassword..'~'..userLVL)
+		local message, distance, sender = net:receive()
+		return message
+	end,
+	removeAccount = function(authToken, password, username)
+		net:send('removeAccount~'..authToken..'~'..password..'~'..username)
+		local message, distance, sender = net:receive()
+		return message
+	end,
+	changeProfile = function(authToken, password, username, data)
+		net:send('changeProfile~'..authToken..'~'..password..'~'..username..'~'..data)
+		local message, distance, sender = net:receive()
+		return message
+	end,
+	changeUserPassword = function(authToken, password, username, newPassword)
+		net:send('changeUserPassword~'..authToken..'~'..password..'~'..username..'~'..newPassword)
+		local message, distance, sender = net:receive()
+		return message
+	end,
+	authTokenLvl = function(authToken)
+		net:send('authTokenLvl~'..authToken)
+		local message, distance, sender = net:receive()
+		return message
+	end}
 
 -- Mail API
 
-function sendMail(to, authToken, topic, body)
-	net:send('sendMail~'..to..'~'..authToken..'~'..topic..'~'..body)
-	local message, distance, sender = net:receive()
-	return message
-end
-
-function showInboxHistory(authToken)
-	net:send('showInboxHistory~'..authToken)
-	local message, distance, sender = net:receive()
-	return message
-end
-
-function showInbox(authToken)
-	net:send('showInbox~'..authToken)
-	local message, distance, sender = net:receive()
-	return message
-end
-
-function readMail(authToken, mailName)
-	net:send('readMail~'..authToken..'~'..mailName)
-	local message, distance, sender = net:receive()
-	return message
-end
-
-function deleteMail(authToken, mailName)
-	net:send('deleteMail~'..authToken..'~'..mailName)
-	local message, distance, sender = net:receive()
-	return message
-end
-
-function showUserInbox(authToken, username)
-	net:send('showUserInbox~'..authToken..'~'..username)
-	local message, distance, sender = net:receive()
-	return message
-end
-
-function showUserInboxHistory(authToken, username)
-	net:send('showUserInboxHistory~'..authToken..'~'..username)
-	local message, distance, sender = net:receive()
-	return message
-end
-
-function readUserMail(authToken, username, mailName)
-	net:send('readUserMail~'..authToken..'~'..username..'~'..mailName)
-	local message, distance, sender = net:receive()
-	return message
-end
-
-function deleteUserMail(authToken, username, mailName)
-	net:send('deleteUserMail~'..authToken..'~'..username..'~'..mailName)
-	local message, distance, sender = net:receive()
-	return message
-end
+local mail = {
+	sendMail = function(to, authToken, topic, body)
+		net:send('sendMail~'..to..'~'..authToken..'~'..topic..'~'..body)
+		local message, distance, sender = net:receive()
+		return message
+	end,
+	showInboxHistory = function(authToken)
+		net:send('showInboxHistory~'..authToken)
+		local message, distance, sender = net:receive()
+		return message
+	end,
+	showInbox = function(authToken)
+		net:send('showInbox~'..authToken)
+		local message, distance, sender = net:receive()
+		return message
+	end,
+	readMail = function(authToken, mailName)
+		net:send('readMail~'..authToken..'~'..mailName)
+		local message, distance, sender = net:receive()
+		return message
+	end,
+	deleteMail = function(authToken, mailName)
+		net:send('deleteMail~'..authToken..'~'..mailName)
+		local message, distance, sender = net:receive()
+		return message
+	end,
+	showUserInbox = function(authToken, username)
+		net:send('showUserInbox~'..authToken..'~'..username)
+		local message, distance, sender = net:receive()
+		return message
+	end,
+	showUserInboxHistory = function(authToken, username)
+		net:send('showUserInboxHistory~'..authToken..'~'..username)
+		local message, distance, sender = net:receive()
+		return message
+	end,
+	readUserMail = function(authToken, username, mailName)
+		net:send('readUserMail~'..authToken..'~'..username..'~'..mailName)
+		local message, distance, sender = net:receive()
+		return message
+	end,
+	deleteUserMail = function(authToken, username, mailName)
+		net:send('deleteUserMail~'..authToken..'~'..username..'~'..mailName)
+		local message, distance, sender = net:receive()
+		return message
+	end}
 
 -- News API
 
-function readNews(title)
-	net:send('readNews~'..title)
-	local message, distance, sender = net:receive()
-	return message
-end
-
-function showNews()
-	net:send('showNews')
-	local message, distance, sender = net:receive()
-	return message
-end
-
-function addArticle(authToken, title, text)
-	net:send('addArticle~'..authToken..'~'..title..'~'..text)
-	local message, distance, sender = net:receive()
-	return message
-end
-
-function removeArticle(authToken, title)
-	net:send('removeArticle~'..authToken..'~'..title)
-	local message, distance, sender = net:receive()
-	return message
-end
-
-function updateArticle(authToken, title, text)
-	net:send('updateArticle~'..authToken..'~'..title..'~'..text)
-	local message, distance, sender = net:receive()
-	return message
-end
+local news = {
+	readNews = function(title)
+		net:send('readNews~'..title)
+		local message, distance, sender = net:receive()
+		return message
+	end,
+	showNews = function()
+		net:send('showNews')
+		local message, distance, sender = net:receive()
+		return message
+	end,
+	addArticle = function(authToken, title, text)
+		net:send('addArticle~'..authToken..'~'..title..'~'..text)
+		local message, distance, sender = net:receive()
+		return message
+	end,
+	removeArticle = function(authToken, title)
+		net:send('removeArticle~'..authToken..'~'..title)
+		local message, distance, sender = net:receive()
+		return message
+	end,
+	updateArticle = function(authToken, title, text)
+		net:send('updateArticle~'..authToken..'~'..title..'~'..text)
+		local message, distance, sender = net:receive()
+		return message
+	end}
